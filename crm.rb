ad97f9b37d4cbc979567b42c9ec40c80f32cd0e1
Paul Walsh <twentytwentyk@live.com>
2017-01-24 13:02:49 -0500

    Implement Add a New Contact

--- crm.rb
+++ crm.rb
@@ -2,6 +2,7 @@
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
 require_relative 'contact'
+
 require 'sinatra'
 
 Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
@@ -11,4 +12,16 @@ Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 get '/' do
   @crm_app_name = "Bitmaker's CRM"
   erb :index
+end
+
+get '/' do
+
+end
+
+get '/contacts' do
+
+end
+
+get '/contacts/new' do
+  erb :new_contact
 end
