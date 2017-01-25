7064923500da2a803c1190442ea638505cf6b2cf
Paul Walsh <twentytwentyk@live.com>
2017-01-25 17:09:41 -0500

    upload Part 2 of assignment

--- contact.rb
+++ contact.rb
@@ -1,9 +1,3 @@
-# Copy your entire Contact class from the old crm assignment here.
-# Then, run the following at the command line:
-#
-#   ruby test/contact_test.rb
-#
-# If your implementation of the Contact class is 'right', then you should see that all tests pass!
 require './test/contact_test.rb'
 
 class Contact
@@ -26,16 +20,12 @@ class Contact
 
     @@id += 1
   end
-  #
+
   def self.create(first_name, last_name, email, note)
     new_contact = self.new(first_name, last_name, email, note)
     @@contacts << new_contact
     new_contact
   end
-  #
-
-  # This method should accept an id as an argument
-  # and return the contact who has that id
 
   def self.find(id)
     @@contacts.each do |contact|
@@ -45,13 +35,7 @@ class Contact
     end
   end
 
-  # This method should allow you to specify
-  # 1. which of the contact's attributes you want to update
-  # 2. the new value for that attribute
-  # and then make the appropriate change to the contact
   def update(attribute, value)
-    # puts value
-    # puts value.class
     case attribute
     when 'first_name'
        @first_name = value
@@ -63,27 +47,7 @@ class Contact
        @note = value
     end
   end
-  #
-  #   puts "What attribute would you like to update? Please enter: first_name, last_name, email, or note. Then enter a new value."
-  #   attribute = gets.chomp
-  #   new_value = gets.chomp
-  #   case attribute
-  #      when "first_name"
-  #        self.first_name = new_value
-  #      when "last_name"
-  #        self.last_name = new_value
-  #      when "email"
-  #        self.email = new_value
-  #      when "note"
-  #        self.note = new_value
-  #   end
-  #   return self
-  # end
 
-  # This method should work similarly to the find method above
-  # but it should allow you to search for a contact using attributes other than id
-  # by specifying both the name of the attribute and the value
-  # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
   def self.find_by(attribute, value)
       case attribute
       when 'first_name'
@@ -116,9 +80,7 @@ class Contact
         "No match for #{value}"
       end
   end
-# when attribute is "first_name" and value is "Betty", find all frist names matching Betty.
-  #
-  # This method should delete all of the contacts
+
   def self.delete_all
     @@contacts = []
   end
@@ -126,9 +88,6 @@ class Contact
   def full_name
     return "#{first_name} #{last_name}"
   end
-  #
-  # # This method should delete the contact
-  # # HINT: Check the Array class docs for built-in methods that might be useful here
 
   def delete
     @@contacts.delete(self)
--- crm.rb
+++ crm.rb
@@ -1,9 +1,9 @@
-# Implement the new web-based CRM here.
-# Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
-# You'll have to implement it from scratch.
+
 require_relative 'contact'
 require 'sinatra'
 
+Contact.create('Paul', 'Walsh', 'twentytwentyk@live.com', 'Local Bard')
+Contact.create('John', 'Petrucci', 'bearucci@gmail.com', 'Local Bear')
 Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
 Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
 Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
